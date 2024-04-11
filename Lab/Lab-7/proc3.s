0d76c58408bf16275900fe812065de69360bf39a
Aryaan <mishraaryaan5@gmail.com>
2024-04-11 16:32:10 -0700

    LAB-7

--- Lab/Lab-7/proc3.s
+++ Lab/Lab-7/proc3.s
@@ -1,97 +1,96 @@
-.data
-newline: .asciiz "\n"
+	.data
+x:	.word 2
+y:	.word 3
+z:	.word 4
+p:	.word 0
+q:	.word 0
 
-.text
-.globl main
+	.text
+MAIN:
+	la $t0, x		#Reads x value for creating a register
+	lw $s0, 0($t0)		#Stores x value
+	la $t0, y		#Reads y value for creating a register
+	lw $s1, 0($t0)		#Stores y value
+	la $t0, z		#Reads z value for creating a register
+	lw $s2, 0($t0)
+	
+	add $a0, $s0, $zero
+	add $a1, $s1, $zero
+	add $a2, $s2, $zero
+	
+	jal FOO
+	
+	add $t1, $s0, $s1	#Adds x and y
+	add $t2, $s2, $v0	#Adds z and foo(x+y+z)
+	add $s2, $t1, $t2	#Adds sum of x and y with sum of z and foo(x+y+z)
+	
+	add $a0, $s2, $zero
+	
+	li $v0, 1 
+	
+	syscall
+	j END
+	
+BAR:
+	sub $t1, $a2, $a0	#Subtracts a from c
+	sllv $v0, $t1, $a1	#Bit shifts by b's value
+	jr $ra
 
-main:
-    # Initialize x, y, z
-    li $s0, 2   # x = 2
-    li $s1, 4   # y = 4
-    li $s2, 6   # z = 6
-
-    # Call foo(x, y, z)
-    move $a0, $s0
-    move $a1, $s1
-    move $a2, $s2
-    jal foo
-
-    # z = x + y + z + foo(x, y, z)
-    add $s2, $s2, $s0
-    add $s2, $s2, $s1
-    add $s2, $s2, $v0
-
-    # Print z
-    move $a0, $s2
-    li $v0, 1
-    syscall
-
-    # Print newline
-    li $v0, 4
-    la $a0, newline
-    syscall
-
-    # Exit
-    li $v0, 10
-    syscall
-
-foo:
-    # Allocate stack frame
-    addi $sp, $sp, -12
-    sw $ra, 8($sp)
-    sw $s0, 4($sp)
-    sw $s1, 0($sp)
-
-    # Call bar(m + o, n + o, m + n)
-    add $t0, $a0, $a2  # m + o
-    add $t1, $a1, $a2  # n + o
-    add $t2, $a0, $a1  # m + n
-    move $a0, $t0
-    move $a1, $t1
-    move $a2, $t2
-    jal bar
-    move $s0, $v0      # p = bar(m + o, n + o, m + n)
-
-    # Call bar(m - o, n - m, n + n)
-    sub $t0, $a0, $a2  # m - o
-    sub $t1, $a1, $a0  # n - m
-    add $t2, $a1, $a1  # n + n
-    move $a0, $t0
-    move $a1, $t1
-    move $a2, $t2
-    jal bar
-    move $s1, $v0      # q = bar(m - o, n - m, n + n)
-
-    # Print p + q
-    add $a0, $s0, $s1
-    li $v0, 1
-    syscall
-
-    # Print newline
-    li $v0, 4
-    la $a0, newline
-    syscall
-
-    # Return p + q
-    add $v0, $s0, $s1
-
-    # Restore registers and return
-    lw $s1, 0($sp)
-    lw $s0, 4($sp)
-    lw $ra, 8($sp)
-    addi $sp, $sp, 12
-    jr $ra
-
-bar:
-    # Allocate stack frame
-    addi $sp, $sp, -4
-    sw $ra, 0($sp)
-
-    # Compute (b - a) << c
-    sub $t0, $a1, $a0  # b - a
-    sllv $v0, $t0, $a2 # (b - a) << c
-
-    # Restore register and return
-    lw $ra, 0($sp)
-    addi $sp, $sp, 4
-    jr $ra
+FOO:
+	addi $sp, $sp, -20	#Backsup
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	sw $a1, 8($sp)
+	sw $a2, 12($sp)
+	sw $s0, 16($sp)
+	
+	add $a0, $s0, $s1	
+	add $a1, $s1, $s2
+	add $a2, $s2, $s0
+	
+	jal BAR			#Calls bar function
+	
+	la $t0, p
+	lw $s0, 0($t0)
+	
+	add $t2, $s0, $v0
+	
+	lw $ra, 0($sp)
+	lw $a0, 4($sp)
+	lw $a1, 8($sp)
+	lw $a2, 12($sp)
+	lw $s0, 16($sp)
+	addi $sp, $sp, 20	#Restore
+	
+	addi $sp, $sp, -20	#Backsup
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	sw $a1, 8($sp)
+	sw $a2, 12($sp)
+	sw $s1, 16($sp)
+	
+	sub $a0, $s0, $s2
+	sub $a1, $s1, $s0
+	mul $a2, $s1, 2
+	
+	jal BAR			#Calls bar function
+	
+	la $t0, q
+	lw $s1, 0($t0)
+	
+	add $t3, $s1, $v0
+	
+	lw $ra, 0($sp)
+	lw $a0, 4($sp)
+	lw $a1, 8($sp)
+	lw $a2, 12($sp)
+	lw $s1, 16($sp)
+	addi $sp, $sp, 20	#Restores
+	
+	add $v0, $t2, $t3
+	
+	jr $ra			#Returns back to main
+	
+END:
+	li $v0, 10
+	syscall
